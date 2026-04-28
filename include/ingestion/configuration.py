1355b362229c7e52cc549b957b94038680c57610
Nathaniel Jonathan Rusli <omgitsnathaniels@gmail.com>
2026-04-28 23:54:32 +0700

    feat: add risk-free rate scraping url

--- include/ingestion/configuration.py
+++ include/ingestion/configuration.py
@@ -7,5 +7,7 @@ class Configuration:
         ]
 
         self.BUCKET_NAME = ""
+        
+        self.RISK_FREE_RATE_URL = "https://tradingeconomics.com/indonesia/government-bond-yield"
 
 configuration = Configuration()
