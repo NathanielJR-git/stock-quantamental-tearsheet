f9e31a892089f9595a37d4465f58291919555f09
0xNathaniel <13523113@std.stei.itb.ac.id>
2026-04-28 21:00:33 +0700

    feat: add testing dag script with taskflow api

--- dags/pipeline.py
+++ dags/pipeline.py
@@ -1,1 +1,20 @@
-# TO-DO: write Airflow DAG
+import pandas as pd
+import pendulum
+from airflow.sdk import dag, task
+from include.ingestion.yfinance_pull import download_stocks_data
+
+@dag(
+    schedule=None,
+    start_date=pendulum.datetime(2026, 4, 28, tz="Asia/Jakarta"),
+    catchup=False,
+    tags=["Stock Quantamental Tearsheet Pipeline DAG"]
+)
+def pipeline():
+    @task
+    def test_yfinance_pull():
+        stocks_data = download_stocks_data()
+        stocks_data.to_csv("/opt/airflow/include/test.csv", index=False)
+
+    test_yfinance_pull()
+
+pipeline()
