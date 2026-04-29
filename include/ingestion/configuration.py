04770f7ab56cb11a45509624211e96efe90ee8ca
0xNathaniel <13523113@std.stei.itb.ac.id>
2026-04-29 12:13:26 +0700

    feat: add more tickers and static company profiles list

--- include/ingestion/configuration.py
+++ include/ingestion/configuration.py
@@ -1,13 +1,41 @@
 class Configuration:
     def __init__(self):
+        # Selected stock tickers (IDX)
         self.TICKERS = [
-            "BBCA.JK",
-            "BMRI.JK",
-            "BBRI.JK"
+            "BBCA.JK", 
+            "BBRI.JK", 
+            "BMRI.JK", 
+            "PTRO.JK", 
+            "RAJA.JK", 
+            "WIFI.JK", 
+            "ADMR.JK", 
+            "ARCI.JK", 
+            "BULL.JK", 
+            "MBSS.JK", 
+            "DEWA.JK", 
+            "BUMI.JK"
         ]
 
+        # Company profiles static data for initial loading
+        self.STATIC_COMPANY_PROFILES = [
+            {"ticker": "BBCA.JK", "company_name": "PT Bank Central Asia Tbk", "sector": "Financials", "industry": "Banks - Regional"},
+            {"ticker": "BBRI.JK", "company_name": "PT Bank Rakyat Indonesia (Persero) Tbk", "sector": "Financials", "industry": "Banks - Regional"},
+            {"ticker": "BMRI.JK", "company_name": "PT Bank Mandiri (Persero) Tbk", "sector": "Financials", "industry": "Banks - Regional"},
+            {"ticker": "PTRO.JK", "company_name": "PT Petrosea Tbk", "sector": "Energy", "industry": "Oil & Gas Equipment & Services"},
+            {"ticker": "RAJA.JK", "company_name": "PT Rukun Raharja Tbk", "sector": "Energy", "industry": "Oil & Gas Midstream"},
+            {"ticker": "WIFI.JK", "company_name": "PT Solusi Sinergi Digital Tbk", "sector": "Technology", "industry": "Information Technology Services"},
+            {"ticker": "ADMR.JK", "company_name": "PT Adaro Minerals Indonesia Tbk", "sector": "Basic Materials", "industry": "Coking Coal"},
+            {"ticker": "ARCI.JK", "company_name": "PT Archi Indonesia Tbk", "sector": "Basic Materials", "industry": "Gold"},
+            {"ticker": "BULL.JK", "company_name": "PT Buana Lintas Lautan Tbk", "sector": "Industrials", "industry": "Marine Shipping"},
+            {"ticker": "MBSS.JK", "company_name": "PT Mitrabahtera Segara Sejati Tbk", "sector": "Industrials", "industry": "Marine Shipping"},
+            {"ticker": "DEWA.JK", "company_name": "PT Darma Henwa Tbk", "sector": "Energy", "industry": "Thermal Coal"},
+            {"ticker": "BUMI.JK", "company_name": "PT Bumi Resources Tbk", "sector": "Energy", "industry": "Thermal Coal"}
+        ]
+
+        # S3 bucket name
         self.BUCKET_NAME = ""
         
+        # Investing.com URL for fetching risk-free rate data
         self.RISK_FREE_RATE_URL = "https://tradingeconomics.com/indonesia/government-bond-yield"
 
 configuration = Configuration()
