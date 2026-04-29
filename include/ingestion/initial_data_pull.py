d00d770cd593142088504a1116d7dd03b3a9b5f5
0xNathaniel <13523113@std.stei.itb.ac.id>
2026-04-29 12:17:12 +0700

    chore: add airflow logging

--- include/ingestion/initial_data_pull.py
+++ include/ingestion/initial_data_pull.py
@@ -35,6 +35,7 @@ def download_historical_ohlcv_data():
             bucket_name=configuration.BUCKET_NAME, 
             replace=True
         )
+    print("Done fetching initial stocks data (5 Years)")
 
 
 def download_company_profiles():
@@ -51,3 +52,4 @@ def download_company_profiles():
         bucket_name=configuration.BUCKET_NAME,
         replace=True
     )
+    print("Done fetching company profiles")
