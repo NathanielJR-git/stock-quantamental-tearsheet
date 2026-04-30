6183fa749d67fbcbd7bf9df516210261058f1f75
Nathaniel Jonathan Rusli <omgitsnathaniels@gmail.com>
2026-04-30 19:27:46 +0700

    refactor: remove yfinance news pull script as now news are only pulled from gnews

--- include/ingestion/news_pull.py
+++ include/ingestion/news_pull.py
@@ -8,53 +8,6 @@ from include.ingestion.configuration import configuration
 from gnews import GNews
 
 
-def download_news_from_yfinance(**kwargs):
-    """
-    Downloads news data for all tickers from yfinance (Yahoo Finance)
-    """
-    print("Starts fetching stocks news data from yfinance")
-    s3_hook = S3Hook(aws_conn_id='aws_default')
-    ds = kwargs.get('ds')
-    if not ds:
-        raise ValueError("Macro {{ds}} is not found, make sure function is called via PythonOperator(provide_context=True)")
-    year, month, day = ds.split("-")
-    month = int(month)
-    day = int(day)
-   
-    for ticker in configuration.TICKERS:
-        ticker_news = yf.Ticker(ticker)
-        news = ticker_news.news
-            
-        # Check if news is empty
-        if not news:
-            continue
-
-        extracted_news = []
-        # Load news
-        for news_item in news:
-            content = news_item.get('content')
-            if not content:
-                continue
-            
-            extracted_news.append({
-                "title": content.get("title"),
-                "summary": content.get("summary"),
-                "publication_date": content.get("pubDate")
-            })
-            
-        # Save news data to S3
-        s3_key = f"bronze/news_data/ticker={ticker}/year={year}/month={month:02d}/day={day:02d}/historical_data.json"
-        s3_hook.load_string(
-            string_data=json.dumps(extracted_news), 
-            key=s3_key, 
-            bucket_name=configuration.BUCKET_NAME, 
-            replace=True
-        )
-
-    print("Done fetching stocks news data from yfinance")
-
-
-
 def download_news_from_gnews(**kwargs):
     """
     Downloads news data for all tickes from gnews (Google News)
