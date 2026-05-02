14813dabba76f2049bdcfb86d5c1d13cbe29c89f
0xNathaniel <13523113@std.stei.itb.ac.id>
2026-05-02 15:45:13 +0700

    del

--- include/pyspark/template.py
+++
@@ -1,8 +0,0 @@
-from airflow.sdk import task
-from pyspark.sql import SparkSession
-from pyspark.context import SparkContext
-import pyspark.sql.functions as F
-
-@task.pyspark(conn_id="spark_default")
-def separated_example(spark: SparkSession):
-    ...
