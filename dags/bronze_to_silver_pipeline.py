151c45bbc757d3d713922106acefc317c1cbb74c
0xNathaniel <13523113@std.stei.itb.ac.id>
2026-05-02 15:34:25 +0700

    feat: add broze to silver transformation dags

--- dags/bronze_to_silver_pipeline.py
+++ dags/bronze_to_silver_pipeline.py
@@ -1,7 +1,8 @@
 import pendulum
+from airflow.hooks.base import BaseHook
 from airflow.sdk import dag, task
-from include.ingestion.configuration import configuration
-from include.pyspark.template import separated_example
+from include.configuration import configuration
+from include.pyspark.bronze_to_silver import *
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
 
@@ -12,13 +13,72 @@ from pyspark.sql import SparkSession
     catchup=True,
     tags=["bronze", "market_data", "news_data"]
 )
-def pipeline():
+def initial_load_pipeline():
+    # Spark builder and Spark context setup
+    spark = SparkSession.builder \
+        .appName("bronze-to-silver-compute") \
+        .getOrCreate()
+
+    # Use Airflow aws_default connection
+    aws_conn = BaseHook.get_connection("aws_default")
+    aws_access_key = aws_conn.login
+    aws_secret_key = aws_conn.password
     
-    @task.pyspark(conn_id="spark_default")
-    def template(spark: SparkSession, sc: SparkContext):
-        ...
+    # Inject Airflow credentials to Hadoop Spark
+    hadoop_conf = spark.sparkContext._jsc.hadoopConfiguration()
+    hadoop_conf.set("fs.s3a.access.key", aws_access_key)
+    hadoop_conf.set("fs.s3a.secret.key", aws_secret_key)
+    hadoop_conf.set("fs.s3a.endpoint", "s3.ap-southeast-3.amazonaws.com")
+    hadoop_conf.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
 
-    template()
-    separated_example() # or just call a separated pyspark task right away
+    # Spark tasks
+    transform_company_profiles(spark)
 
-pipeline()
+
+def daily_pipeline():
+    # Spark builder and Spark context setup
+    spark = SparkSession.builder \
+        .appName("bronze-to-silver-compute") \
+        .getOrCreate()
+    sc = spark.sparkContext
+
+    # Use Airflow aws_default connection
+    aws_conn = BaseHook.get_connection("aws_default")
+    aws_access_key = aws_conn.login
+    aws_secret_key = aws_conn.password
+    
+    # Inject Airflow credentials to Hadoop Spark
+    hadoop_conf = sc._jsc.hadoopConfiguration()
+    hadoop_conf.set("fs.s3a.access.key", aws_access_key)
+    hadoop_conf.set("fs.s3a.secret.key", aws_secret_key)
+    hadoop_conf.set("fs.s3a.endpoint", "s3.ap-southeast-3.amazonaws.com")
+    hadoop_conf.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+
+    # Spark tasks
+    transform_company_profiles(spark, sc)
+
+
+def weekly_pipeline():
+    # Spark builder and Spark context setup
+    spark = SparkSession.builder \
+        .appName("bronze-to-silver-compute") \
+        .getOrCreate()
+    sc = spark.sparkContext
+
+    # Use Airflow aws_default connection
+    aws_conn = BaseHook.get_connection("aws_default")
+    aws_access_key = aws_conn.login
+    aws_secret_key = aws_conn.password
+    
+    # Inject Airflow credentials to Hadoop Spark
+    hadoop_conf = sc._jsc.hadoopConfiguration()
+    hadoop_conf.set("fs.s3a.access.key", aws_access_key)
+    hadoop_conf.set("fs.s3a.secret.key", aws_secret_key)
+    hadoop_conf.set("fs.s3a.endpoint", "s3.ap-southeast-3.amazonaws.com")
+    hadoop_conf.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+
+    # Spark tasks
+    transform_company_profiles(spark, sc)
+
+
+initial_load_pipeline()
