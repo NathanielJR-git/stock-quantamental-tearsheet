98392bc28749c25638751a7809d533d7569a3429
0xNathaniel <13523113@std.stei.itb.ac.id>
2026-05-02 15:34:14 +0700

    feat: add transform_company_profiles pyspark task

---
+++ include/pyspark/bronze_to_silver.py
@@ -0,0 +1,58 @@
+from airflow.sdk import task
+from include.configuration import configuration
+from pyspark.sql import SparkSession
+from pyspark.context import SparkContext
+from pyspark.sql.types import StructType, StructField, StringType
+import pyspark.sql.functions as F, col, lower
+
+
+@task.pyspark(conn_id="spark_default")
+def transform_company_profiles(spark: SparkSession):
+    print("Starts company profiles bronze to silver transformation")
+    # Setup schema 
+    company_profiles_schema = StructType([
+        StructField("ticker", StringType(), False),
+        StructField("company_name", StringType(), False),
+        StructField("sector", StringType(), False),
+        StructField("industry", StringType(), False),
+    ])
+
+    # Read company profiles JSON file from S3
+    df_bronze = spark.read \
+        .schema(company_profiles_schema) \
+        .json(configuration.BRONZE_COMPANY_PROFILES_PATH)
+
+    # Transform to silver format
+    df_silver = df_bronze.select(
+        col("ticker"),
+        col("company_name"),
+        lower(col("sector")).alias("sector"),
+        lower(col("industry")).alias("industry")
+    )
+
+    # Save to company profile S3 silver path as Parquet
+    df_silver.write \
+        .mode("overwrite") \
+        .parquet(configuration.SILVER_COMPANY_PROFILES_PATH)
+
+    print(f"Done processing company profile bronze to silver transformation")
+
+
+@task.pyspark(conn_id="spark_default")
+def transform_market_data(spark: SparkSession):
+    ...
+
+
+@task.pyspark(conn_id="spark_default")
+def transform_market_metrics(spark: SparkSession):
+    ...
+
+
+@task.pyspark(conn_id="spark_default")
+def transform_news_data(spark: SparkSession):
+    ...
+
+
+@task.pyspark(conn_id="spark_default")
+def transform_risk_free_rate(spark: SparkSession):
+    ...
