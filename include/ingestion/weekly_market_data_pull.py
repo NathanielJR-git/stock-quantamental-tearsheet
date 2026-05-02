b90a32ad113f56f6bc97c47520b2a1a1abe4f383
0xNathaniel <13523113@std.stei.itb.ac.id>
2026-05-02 18:07:22 +0700

    refactor: revert rff rf key

--- include/ingestion/weekly_market_data_pull.py
+++ include/ingestion/weekly_market_data_pull.py
@@ -110,7 +110,7 @@ def download_risk_free_rate_data(**kwargs):
     
     # Save risk-free rate data to S3
     rf_data = {"risk-free-rate": yield_decimal}
-    rf_key = f"bronze/risk-free-rate/rfr_{year}{month:02d}{day:02d}.json"
+    rf_key = f"bronze/risk-free-rate/year={year}/month={month:02d}/day={day:02d}/risk-free-rate.json"
     
     s3_hook.load_string(
         string_data=json.dumps(rf_data),
