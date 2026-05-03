ce3e1dce41b9eb01d2e7316fefd61d453298b171
0xNathaniel <13523113@std.stei.itb.ac.id>
2026-05-03 18:11:48 +0700

    feat: add latest date filter to stock tearsheet gold transformation

--- include/pyspark/silver_to_gold.py
+++ include/pyspark/silver_to_gold.py
@@ -3,6 +3,7 @@ from include.configuration import configuration
 from include.pyspark.utils import apply_s3_config
 from pyspark.sql import SparkSession
 from pyspark.context import SparkContext
+from pyspark.sql.window import Window
 import pyspark.sql.functions as F
 
 
@@ -34,16 +35,23 @@ def transform_to_stock_tearsheet(spark: SparkSession, sc: SparkContext):
         how="inner"
     )
 
-    df_stock_tearsheet = df_market_and_risk_data.join(
+    df_combined = df_market_and_risk_data.join(
         df_profiles_and_news,
         on=["ticker", "date"],
         how="inner"
     )
 
+    # Take the latest data for each ticker 
+    ticker_max_date_window = Window.partitionBy("ticker").orderBy(F.col("date").desc())
+
+    df_stock_tearsheet = df_combined \
+        .withColumn("row_number", F.row_number().over(ticker_max_date_window)) \
+        .filter(F.col("row_number") == 1) \
+        .drop("row_number")
+        
     # Save to stock tearsheet S3 gold path as Parquet
     df_stock_tearsheet.write \
         .mode("overwrite") \
-        .partitionBy("ticker") \
         .parquet(configuration.GOLD_STOCK_TEARSHEET)
 
     print("Done processing stock tearsheet silver to gold transformation")
